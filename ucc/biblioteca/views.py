39fa20b8da998baf338010c30b65eea20b83aeaf
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-08-23 23:19:14 +0000

    Create new view and new route in biblioteca app

--- ucc/biblioteca/views.py
+++ ucc/biblioteca/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Bienvenido a la biblioteca")
+
+def view_blue(request):
+    return HttpResponse("<h1 style='color:white;background-color:blue;'>Esto esta en un fondo azul</h1>")

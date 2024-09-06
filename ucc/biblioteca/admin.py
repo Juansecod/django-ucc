19b7dce55b2dc69e043ba8cd3859969889fcda74
Juan Sebastian <juan.sebastianri28@gmail.com>
2024-09-06 22:56:32 +0000

    Add props to filter and find in admin platform

--- ucc/biblioteca/admin.py
+++ ucc/biblioteca/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "author", "pages", "publication_date", "classification"]
+    search_fields =["title", "author"]
+    list_filter = ["classification", "publication_date"]
+
+admin.site.register(Book, BookAdmin)
